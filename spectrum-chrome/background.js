03176ab5076be401dc5ecfdd250617ec9d65f558
Martin Yushko <martin.yushko@gmail.com>
2021-01-17 00:07:47 -0800

    Added persistence

--- spectrum-chrome/background.js
+++ spectrum-chrome/background.js
@@ -12,20 +12,54 @@ chrome.tabs.onActivated.addListener(tab => {
 
 var background = {
     activeRebutticles: [],
+    signedIn : false,
 
     init: function() {
+        //temporary local gata
 
-        this.activeRebutticles = rebutticles;
+        class RebutticleData {
+            constructor(bias, opinion, url, title) {
+                this.title = title;
+                this.bias = bias;
+                this.opinion = opinion;
+                this.url = url;
+            }
+        }
+        
+        this.activeRebutticles = [];
+        for (i = 0; i < 10; i++) {
+            data = new RebutticleData(Math.random() * 10, Math.random() * 10, "http://google.com", "Google" + i)
+            this.activeRebutticles.push(data);
+        }
+        
+        //end
+
 
         chrome.runtime.onMessage.addListener(function(request, sender, response) {
             if (request.fn in background) {
                 background[request.fn](request, sender, response);
             }
         });
+
+          console.log("finished background init");
     },
 
     getRebutticles: function(request, sender, response) {
         response(this.activeRebutticles);
-    }
+    },
 
+    getSignInStatus: function(request, sender, response) {
+        response(this.signedIn);
+    },
+
+    removeRebutticle: function(request, sender, response) {
+        this.activeRebutticles.splice(request.i, 1);
+    },
+
+    signIn: function(request, sender, response) {
+        console.log("logging in");
+        this.signedIn = true;
+    }
 }
+
+background.init();
