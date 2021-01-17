938d7050cfdb265cd155a10a056685ca04563e34
Martin Yushko <martin.yushko@gmail.com>
2021-01-17 00:21:35 -0800

    Changing tabs adds to list of cards

--- spectrum-chrome/background.js
+++ spectrum-chrome/background.js
@@ -1,64 +1,68 @@
-chrome.tabs.onActivated.addListener(tab => {
-    chrome.tabs.get(tab.tabId, tab_info => {
-        if (true) {
-            // generate rebutticle(s)
-            // notify user + change icon
-            console.log(tab_info);
-            chrome.storage.local.set({"update": "true"});
-            //createCard(5, 5, 5, tab_info.url, 100, tab_info.title)
-        }
-    });
-});
+class RebutticleData {
+    constructor(bias, opinion, url, title) {
+        this.title = title;
+        this.bias = bias;
+        this.opinion = opinion;
+        this.url = url;
+    }
+}
 
 var background = {
     activeRebutticles: [],
-    signedIn : false,
+    signedIn: false,
 
-    init: function() {
+    init: function () {
         //temporary local gata
 
-        class RebutticleData {
-            constructor(bias, opinion, url, title) {
-                this.title = title;
-                this.bias = bias;
-                this.opinion = opinion;
-                this.url = url;
-            }
-        }
-        
         this.activeRebutticles = [];
         for (i = 0; i < 10; i++) {
             data = new RebutticleData(Math.random() * 10, Math.random() * 10, "http://google.com", "Google" + i)
             this.activeRebutticles.push(data);
         }
-        
+
         //end
 
 
-        chrome.runtime.onMessage.addListener(function(request, sender, response) {
+        chrome.runtime.onMessage.addListener(function (request, sender, response) {
             if (request.fn in background) {
                 background[request.fn](request, sender, response);
             }
         });
 
-          console.log("finished background init");
+        chrome.tabs.onActivated.addListener(tab => {
+            chrome.tabs.get(tab.tabId, tab_info => {
+                if (true) {
+                    // generate rebutticle(s)
+                    this.addRebutticle({url: tab_info.url, title:tab_info.title}, null, null);
+                }
+            });
+        });
+
+        console.log("finished background init");
     },
 
-    getRebutticles: function(request, sender, response) {
+    getRebutticles: function (request, sender, response) {
         response(this.activeRebutticles);
     },
 
-    getSignInStatus: function(request, sender, response) {
+    getSignInStatus: function (request, sender, response) {
         response(this.signedIn);
     },
 
-    removeRebutticle: function(request, sender, response) {
+    removeRebutticle: function (request, sender, response) {
         this.activeRebutticles.splice(request.i, 1);
     },
 
-    signIn: function(request, sender, response) {
+    signIn: function (request, sender, response) {
         console.log("logging in");
         this.signedIn = true;
+    },
+
+    addRebutticle: function (request, sender, response) {
+        console.log("Tab successfully added");
+        data = new RebutticleData(Math.random() * 10, Math.random() * 10, request.url, request.title);
+        this.activeRebutticles.push(data);
+        
     }
 }
 
