2a2d44feec478df57329096250d0fccc510f5f4e
Martin Yushko <martin.yushko@gmail.com>
2021-01-16 20:12:06 -0800

    I'm so tired lol things are broken

--- spectrum-chrome/background.js
+++ spectrum-chrome/background.js
@@ -1,9 +1,31 @@
-/*chrome.tabs.onActivated.addListener(tab => {
-    chrome.tabs.get(tab.tabId, current_tab_info => {
-        if (false) {
+chrome.tabs.onActivated.addListener(tab => {
+    chrome.tabs.get(tab.tabId, tab_info => {
+        if (true) {
             // generate rebutticle(s)
             // notify user + change icon
-            chrome.tabs.executeScript(null, { file: "./foreground.js"});
+            console.log(tab_info);
+            chrome.storage.local.set({"update": "true"});
+            //createCard(5, 5, 5, tab_info.url, 100, tab_info.title)
         }
     });
-});*/
+});
+
+var background = {
+    activeRebutticles: [],
+
+    init: function() {
+
+        this.activeRebutticles = rebutticles;
+
+        chrome.runtime.onMessage.addListener(function(request, sender, response) {
+            if (request.fn in background) {
+                background[request.fn](request, sender, response);
+            }
+        });
+    },
+
+    getRebutticles: function(request, sender, response) {
+        response(this.activeRebutticles);
+    }
+
+}
